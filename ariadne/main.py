c4126d310357fb594fc93cbae3b64e4e6a2b1173
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2021-05-05 14:57:23 -0400

    ENH: copy 'bluesky-widgets-demo' code

---
+++ ariadne/main.py
@@ -0,0 +1,30 @@
+import argparse
+
+from bluesky_widgets.qt import gui_qt
+
+from .viewer import Viewer
+from .settings import SETTINGS
+
+
+def main(argv=None):
+    print(__doc__)
+
+    parser = argparse.ArgumentParser(description="Ariadne: acquisition GUI for NSLS-II BMM beamline")
+    parser.add_argument("--zmq", help="0MQ address")
+    parser.add_argument("--catalog", help="Databroker catalog")
+    args = parser.parse_args(argv)
+
+    with gui_qt("Ariadne"):
+        if args.catalog:
+            import databroker
+
+            SETTINGS.catalog = databroker.catalog[args.catalog]
+
+        # Optional: Receive live streaming data.
+        if args.zmq:
+            SETTINGS.subscribe_to.append(args.zmq)
+        viewer = Viewer()  # noqa: 401
+
+
+if __name__ == "__main__":
+    main()
