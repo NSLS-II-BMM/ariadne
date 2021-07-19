664a88968b67a2d8aa4bc27e6b69551a02415e31
gbischof <bisc8233@gmail.com>
2021-07-19 14:21:46 -0400

    Add catalog fixture

--- ariadne/tests/test_examples.py
+++ ariadne/tests/test_examples.py
@@ -1,8 +1,17 @@
+import pytest
+
 from databroker._drivers.jsonl import BlueskyJSONLCatalog
 
-def test_from_config():
+@pytest.fixture(scope='module')
+def catalog():
     catalog = BlueskyJSONLCatalog(
         "./ariadne/tests/*.jsonl",
         name='bmm')
+    return catalog
+
+def test_from_config(catalog):
+    #catalog = BlueskyJSONLCatalog(
+    #    "./ariadne/tests/*.jsonl",
+    #    name='bmm')
     assert len(catalog)
 
