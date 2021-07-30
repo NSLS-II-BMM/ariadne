374bb64ab0743cb8ec64c3a2e8655f8ccaaf2f53
gbischof <bisc8233@gmail.com>
2021-07-30 12:32:47 -0400

    add functions for the other plots

--- ariadne/plots.py
+++ ariadne/plots.py
@@ -16,8 +16,7 @@ class AutoBMMPlot(AutoPlotter):
                     self.plot_builders.append(model)
                 self.figures.extend(figures)
 
-
-    def rel_scan_linescan_xafs_y_lt(self, run, stream_name):
+    def rel_scan_linescan_xafs_y_it(self, run, stream_name):
         # FIXME: Need to sort out how to get the correct motor here
         x_values = 'xafs_y'
         models = []
@@ -39,7 +38,21 @@ class AutoBMMPlot(AutoPlotter):
 
         return models, figures
 
-    def rel_scan_linescan_xafs_pitch_lt(self, run, stream_name):
+    def rel_scan_linescan_xafs_pitch_it(self, run, stream_name):
+        x_values = run.metadata['start']['motors'][0]
+        models = []
+        figures = []
+
+        axes = Axes()
+        figure = Figure((axes,), title="I0")
+        figures.append(figure)
+        models.append(
+            Lines(x=x_values, ys=['I0',],    max_runs=1, axes=axes)
+        )
+
+        return models, figures
+
+    def rel_scan_linescane_xafs_y_if(self, run, stream_name):
         x_values = run.metadata['start']['motors'][0]
         models = []
         figures = []
@@ -54,6 +67,24 @@ class AutoBMMPlot(AutoPlotter):
         return models, figures
 
     def scan_nd_xafs_trans(self, run, stream_name):
+        x_values = run.metadata['start']['motors'][0]
+        models = []
+        figures = []
+
+        axes1 = Axes()
+        axes2 = Axes()
+        figure = Figure((axes1, axes2), title='It and I0')
+        figures.append(figure)
+        models.append(
+            Lines(x=x_values, ys=['It/I0',], max_runs=1, axes=axes1)
+        )
+        models.append(
+            Lines(x=x_values, ys=['I0',],    max_runs=1, axes=axes2)
+        )
+
+        return models, figures
+
+    def scan_nd_xafs_fluorescence(self, run, stream_name):
         x_values = run.metadata['start']['motors'][0]
         models = []
         figures = []
