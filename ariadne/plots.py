83f98838cd97bef1653acfadb97ac7af430de433
gbischof <bisc8233@gmail.com>
2021-07-30 17:29:04 -0400

    touch ups

--- ariadne/plots.py
+++ ariadne/plots.py
@@ -13,7 +13,9 @@ class AutoBMMPlot(AutoPlotter):
         if stream_name == 'primary':
 
             plan_name = run.metadata['start'].get('plan_name').split(' ')
-            plan = plan_name[1] # xafs or linescan
+            if len(plan_name) > 1:
+                plan = plan_name[1] # xafs or linescan
+
             subtype = plan_name[-1] # trans, ref, fluorescence, I0, It, Ir
 
             if plan == 'linescan':
@@ -21,23 +23,22 @@ class AutoBMMPlot(AutoPlotter):
             elif plan == 'xafs':
                 x_axis = 'dcm_energy'
             else:
-                return
+                return  # Only make plots for linescan and xafs.
 
-            try:
-                element = run.metadata['start']['XDI']['Element']['Symbol']
+            element = run.metadata['start'].get('XDI', {}).get('Element',{}).get('Symbol')
+            if element is not None:
                 fluorescence_signal = f'{element}1+{element}2+{element}3+{element}4'
-            except KeyError:
-                element = None
+            else:
                 fluorescence_signal = None
 
             plot_lookup = {'I0': ['I0'],
-               'It': ['It/I0'],
-               'Ir': ['Ir/It'],
-               'If': [f'({fluorescence_signal})/I0'],
-               'trans': ['log(I0/It)', 'log(It/Ir)', 'I0', 'It/I0', 'Ir/It'],
-               'fluorescence': [f'({fluorescence_signal})/I0', 'log(I0/It)',
-                                 'log(It/Ir)', 'I0', 'It/I0', 'Ir/It'],
-               'ref': ['log(It/Ir)', 'It/I0', 'Ir/It']}
+                           'It': ['It/I0'],
+                           'Ir': ['Ir/It'],
+                           'If': [f'({fluorescence_signal})/I0'],
+                           'trans': ['log(I0/It)', 'log(It/Ir)', 'I0', 'It/I0', 'Ir/It'],
+                           'fluorescence': [f'({fluorescence_signal})/I0', 'log(I0/It)',
+                                           'log(It/Ir)', 'I0', 'It/I0', 'Ir/It'],
+                           'ref': ['log(It/Ir)', 'It/I0', 'Ir/It']}
 
             for y_axis in plot_lookup[subtype]:
                 title = ' '.join(plan_name)
